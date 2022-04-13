7f38b9389eb216699335b1b004136b0747db7233
all-day-and-night <jwug0821@gmail.com>
2022-04-13 20:24:02 +0900

    Security access restrict

--- json_web_tokens/jwt.tutorial/src/main/java/me/security/jwt/config/SecurityConfig.java
+++ json_web_tokens/jwt.tutorial/src/main/java/me/security/jwt/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package me.security.jwt.tutorial.config;
+package me.security.jwt.config;
 
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,7 +12,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests() // httpServletRequest를 사용하는 요청들에 대한 접근 제한을 설정하겠다는 의미
-                .antMatchers("/api/hello").permitAll() // /api/hello에 대한 요청은 인증없이 접근을 허용
+                .antMatchers("/api/hello", "/").permitAll() // /api/hello에 대한 요청은 인증없이 접근을 허용
                 .anyRequest().authenticated(); // 나머지 요청들에 대해서는 인증을 받아야 한다.
     }
 }
